c49433ceeb655db07f92bc93ab6c4d226f81f05b
Wicked_Colours <sandra.w.l@gmail.com>
2025-04-16 12:54:58 +0200

    FontComponent start

---
+++ frontend-mobile/flood-app/components/FontComponent.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FontComponent = () => {
+  return (
+    <View>
+      <Text>FontComponent</Text>
+    </View>
+  )
+}
+
+export default FontComponent
+
+const styles = StyleSheet.create({})
