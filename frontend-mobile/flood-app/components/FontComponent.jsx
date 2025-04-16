1a2aa7b5afb2e16b1b36073b245c2b49029d3b8f
Wicked_Colours <sandra.w.l@gmail.com>
2025-04-16 14:29:32 +0200

    Start TextComponent

--- frontend-mobile/flood-app/components/FontComponent.jsx
+++ frontend-mobile/flood-app/components/FontComponent.jsx
@@ -1,14 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context'
 
 const FontComponent = () => {
+    const [titleText, setTitleText] = useState("Exempeltext");
+    const bodyText = 'This is not really a bird nest.';
+    const onPressTitle = () => {
+      setTitleText("Test [pressed]");
+    };
+
   return (
-    <View>
-      <Text>FontComponent</Text>
-    </View>
-  )
-}
+   <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Text>
+      <Text style={styles.titleText} onPress={onPressTitle}>
+        {titleText}
+        {'\n'}
+        {'\n'}
+      </Text>
+      <Text numberOfLines={5}>{bodyText}</Text>
+      </Text>
+    </SafeAreaView>
+   </SafeAreaProvider>>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  // baseText: {
+  //   fontFamily: ?
+  // },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
 
 export default FontComponent
 
-const styles = StyleSheet.create({})
