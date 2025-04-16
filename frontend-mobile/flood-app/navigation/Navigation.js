130fdb1d251f545b579cd642fee5c94c5aa82243
atfedmis <federicamisirocchi@hotmail.com>
2025-04-16 10:38:56 +0200

    add Card component for improved layout and theme integration

--- frontend-mobile/flood-app/navigation/Navigation.js
+++ frontend-mobile/flood-app/navigation/Navigation.js
@@ -46,8 +46,6 @@ const Navigation = () => {
             }}
 
             />
-         
-
         </Tab.Navigator>
     </NavigationContainer>
   )
--- frontend-mobile/flood-app/screens/HomeScreen.jsx
+++ frontend-mobile/flood-app/screens/HomeScreen.jsx
@@ -3,38 +3,46 @@ import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { ScrollView } from 'react-native';
 import HeroImage from '../components/HeroImage'
 import { useTheme } from "../themes/ThemeContext";
-import { MaterialCommunityIcons} from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import Card from '../components/Card';
 
 const HomeScreen = () => {
   const { theme, isDark, toggleTheme } = useTheme();
 
   return (
     <ScrollView style={[styles.container, { backgroundColor: theme.background }]}>
-        <View style={styles.header}>
-            <Text>Test</Text>
-           
-          <Pressable onPress={toggleTheme}>
+      <View style={styles.header}>
+        <Text>Test</Text>
+
+        <Pressable onPress={toggleTheme}>
           <MaterialCommunityIcons
-          name={isDark? "white-balance-sunny" : "weather-night"}
-          color={theme.accent}
-          size={24}
-          
+            name={isDark ? "white-balance-sunny" : "weather-night"}
+            color={theme.accent}
+            size={24}
+
           />
-         </Pressable>
-        </View>
-      <HeroImage/>
+        </Pressable>
+      </View>
+      <HeroImage />
       <View>
         <Text>
           Testar lite till
         </Text>
       </View>
 
+      <View style={styles.cardContainer}>
+        <Card title="1" width="60%" height={100} />
+        <Card title="2" width="60%" height={100} />
+        <Card title="3" width="60%" height={100} />
+        <Card title="4" width="60%" height={100} />
+      </View>
+
     </ScrollView>
   )
 }
- export default HomeScreen
+export default HomeScreen
 
- const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
@@ -44,5 +52,10 @@ const HomeScreen = () => {
     alignItems: "center",
     paddingHorizontal: 20,
     paddingVertical: 1
+  }, cardContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    paddingVertical: 16,
   }
- })
+})
