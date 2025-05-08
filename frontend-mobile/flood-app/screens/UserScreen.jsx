e978d728294219ae195f9f7f69b8cfd3b4713dee
atfedmis <federicamisirocchi@hotmail.com>
2025-05-08 11:10:48 +0200

    refactor UserScreen to include WorkerStatus and MapScreen components with updated styles

--- frontend-mobile/flood-app/screens/UserScreen.jsx
+++ frontend-mobile/flood-app/screens/UserScreen.jsx
@@ -1,14 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import WorkerStatus from '../components/WorkerStatus'
+import MapScreen from '../components/MapScreen'
+import * as Location from 'expo-location'
 
 const UserScreen = () => {
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.statusContainer}>
+        <WorkerStatus />
+      </View>
+      <View style={styles.mapContainer}>
+        <MapScreen />
+      </View>
     </View>
   )
 }
 
 export default UserScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  statusContainer: {
+    zIndex: 1,
+  },
+  mapContainer: {
+    flex: 1,
+  }
+})
