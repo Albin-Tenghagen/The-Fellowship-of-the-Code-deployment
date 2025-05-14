36c92d6340ed094ced10b8e0c369b9dbffd2462c
Wicked_Colours <sandra.w.l@gmail.com>
2025-05-14 21:00:35 +0200

    locationCard

---
+++ frontend-mobile/flood-app/components/LocationCard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LocationCard = () => {
+  return (
+    <View>
+      <Text>LocationCard</Text>
+    </View>
+  )
+}
+
+export default LocationCard
+
+const styles = StyleSheet.create({})
