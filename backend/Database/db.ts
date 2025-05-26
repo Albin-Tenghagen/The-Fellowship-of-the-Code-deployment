fabf502d5a661f53cab5d1eb5c40c32f5001a99d
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-05-27 01:58:54 +0200

    console.log for error handling

--- backend/Database/db.ts
+++ backend/Database/db.ts
@@ -7,7 +7,13 @@ const { Pool } = pg;
 
 // Railway provides a single DATABASE_URL for connection, so prefer that:
 const connectionString = process.env.DATABASE_URL;
-
+console.log("Connecting to DB with:", {
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  host: process.env.DBHOST,
+  port: process.env.DBPORT,
+});
 const pool = connectionString
   ? new Pool({
       connectionString,
@@ -27,13 +33,6 @@ const testConnection = async () => {
   try {
     const res = await pool.query("SELECT * FROM admins");
     console.log("Connected! admins for DB:", res.rows[0]);
-    console.log("Connecting to DB with:", {
-      user: process.env.DBUSER,
-      password: process.env.DBPASSWORD,
-      database: process.env.DBNAME,
-      host: process.env.DBHOST,
-      port: process.env.DBPORT,
-    });
   } catch (error) {
     if (error instanceof Error) {
       console.error("Connection error:", error.message);
