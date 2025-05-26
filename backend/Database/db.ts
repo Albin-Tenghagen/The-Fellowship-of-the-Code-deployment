743fe7395ed0cd4ad7b3bc5fcdea92290a708de7
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2025-05-27 01:38:44 +0200

    changed db.ts and docker compose to accomondate railway

--- backend/Database/db.ts
+++ backend/Database/db.ts
@@ -5,23 +5,13 @@ dotenv.config();
 
 const { Pool } = pg;
 
-// Railway provides a single DATABASE_URL for connection, so prefer that:
-const connectionString = process.env.DATABASE_URL;
-
-const pool = connectionString
-  ? new Pool({
-      connectionString,
-      ssl: {
-        rejectUnauthorized: false, // Important for many hosted DBs like Railway's
-      },
-    })
-  : new Pool({
-      user: process.env.DBUSER,
-      password: process.env.DBPASSWORD,
-      database: process.env.DBNAME,
-      host: process.env.DBHOST,
-      port: Number(process.env.DBPORT),
-    });
+const pool = new Pool({
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  host: process.env.DBHOST,
+  port: Number(process.env.DBPORT),
+});
 
 const testConnection = async () => {
   try {
